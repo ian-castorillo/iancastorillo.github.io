ba4da8073566a905246c5a2788ac1d5f358775ea
Ian Melchor Castorillo <iancastorillo@gmail.com>
2025-07-15 22:31:34 -0700

    microGraphics

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+//rotating ianCastorillo
+const el =document.querySelector('.ianCastorilloRotate');
+const text=el.dataset.text;
+
+el.innerHTML = [...text].map(char => {
+
+  return `<span>${char === ' ' ? '&nbsp;' : char}</span>`;
+}).join('');
