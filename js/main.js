059e2ee563414c755bf12cf14aaae33798b4ab36
Ian Melchor Castorillo <iancastorillo@gmail.com>
2025-07-21 21:48:33 -0700

    niceOne

--- js/main.js
+++ js/main.js
@@ -10,23 +10,4 @@ el.innerHTML = [...text].map(char => {
 }).join('');
 
 
-//glitch effect for white shirt black pants
-  function toggleGlitch(el) {
-    el.classList.toggle('glitch');
-  }
 
-  function randomGlitchLoop() {
-    const elements = document.querySelectorAll('.indented');
-
-    elements.forEach(el => {
-      el.setAttribute('data-text', el.textContent); 
-      if (Math.random() > 0.7) {
-        toggleGlitch(el);
-        setTimeout(() => toggleGlitch(el), Math.random() * 300 + 100); 
-      }
-    });
-
-    setTimeout(randomGlitchLoop, 800); // interval between checks
-  }
-
-  document.addEventListener('DOMContentLoaded', randomGlitchLoop);
